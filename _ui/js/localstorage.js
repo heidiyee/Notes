fad74969716c7e416f1eb56c3150899f0c369e7d
heidiyee <heidi.n.yee@gmail.com>
2017-03-01 16:25:02 -0800

    able to add note to local storage

--- _ui/js/localstorage.js
+++ _ui/js/localstorage.js
@@ -1,3 +1,15 @@
+var noteCount = 0;
+
+var addNotesClicked = function() {
+    $('.add').click(function(e) {
+        var title = $('#title').value;
+        if (typeof title === 'undefined') {
+            title = "title" + noteCount;
+        }
+        addNoteToLocalStorage(title);
+    });
+};
+
 function checkForLocalStorage(){
     try {
         return 'localStorage' in window && window['localStorage'] !== null;
@@ -5,3 +17,10 @@ function checkForLocalStorage(){
         return false;
     }
 }
+
+function addNoteToLocalStorage(title){
+    checkForLocalStorage();
+    localStorage.setItem("note"+noteCount, title);
+}
+
+$(document).ready(addNotesClicked);
